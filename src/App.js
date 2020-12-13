ec4d760a0814f3c6c7a71cebddab109ffdb17ab8
Cindy Zhu <zhucindy1234567890@gmail.com>
2020-12-13 15:17:49 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import {MainDashboard} from './containers/MainDashboard'
 var url = 'http://newsapi.org/v2/top-headlines?' +
           'country=us&' +
           'apiKey=a0031dc43da443b4b59015dc0fd285ac';
-
 var req = new Request(url);
+var news;
+
+fetch(req)
+    .then(function(response) {
+        news = response.json()
+        console.log(news);
+    })
+
+
+
+
+
 //weather api 
 var API_key = '24d935cd21f75fb2c6d74e913dd257cf';
 var city_name = 'New York City, US';
 var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London&appid=24d935cd21f75fb2c6d74e913dd257cf';//'api.openweathermap.org/data/2.5/weather?q='+ {city_name} + '&appid='+ {API_key};
 var wReq = new Request(weatherUrl);
+var weather;
 
 
 fetch(wReq)
     .then(function(response) {
-        console.log(response.json());
+      weather = response.json();
     })
 
+console.log(weather);
 
 function App() {
   return (
